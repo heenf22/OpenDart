e7d18e3d9536f84886f44ed1c986f5ac10c84fd3
Gyuho Lee <heenf22@gmail.com>
2021-09-06 07:56:00 +0000

    add valuation

--- OpenStock/Stock.cs
+++ OpenStock/Stock.cs
@@ -19,8 +19,10 @@ namespace OpenStock
         //---
         public double PresentTotalStockPrise { get; set; }  // 현재 시가총액 = 상장주식수(LastStockCount) x 종가(LastPrise)
         public double FutureTotalStockPrise { get; set; }   // 적정 시가총액 = 영업이익(LastProfit1) x 적정 PER(UserPER)
-        public double LastPER { get; set; }                 // 현재 PER(평균) = 현재 시가총액(PresentTotalStockPrise) / 평균순익(LastProfit3)
-        public double UserPER { get; set; }                 // 적정 PER = 투자자가 결정
+        public double PER1 { get; set; }                // 현재 PER(영업이익) = 현재 시가총액(PresentTotalStockPrise) / 영업이익(LastProfit3)
+        public double PER2 { get; set; }                // 현재 PER(당기순익) = 현재 시가총액(PresentTotalStockPrise) / 당기순익(LastProfit3)
+        public double PER3 { get; set; }                // 현재 PER(평균순익) = 현재 시가총액(PresentTotalStockPrise) / 평균순익(LastProfit3)
+        public double UserPER { get; set; }             // 적정 PER = 투자자가 결정 (default: 평균 PER)
         public double EPS1 { get; set; }                // EPS(영업이익) = 영업이익(LastProfit1) / 상장주식수(LastStockCount)
         public double EPS2 { get; set; }                // EPS(당기순익) = 당기순익(LastProfit1) / 상장주식수(LastStockCount)
         public double EPS3 { get; set; }                // EPS(평균순익) = 평균순익(LastProfit1) / 상장주식수(LastStockCount)
@@ -58,7 +60,9 @@ namespace OpenStock
 
             PresentTotalStockPrise = 0;
             FutureTotalStockPrise = 0;
-            LastPER = 0;
+            PER1 = 0;
+            PER2 = 0;
+            PER3 = 0;
             UserPER = 0;
             EPS1 = 0;
             EPS2 = 0;
@@ -83,6 +87,83 @@ namespace OpenStock
             LastUpdateDT = DateTime.UtcNow;
         }
 
+        // http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020101
+        // [12001] 전종목 시세, csv_file 내용의 라인값을 입력
+        public void initialize(string csvLine)
+        {
+            string[] item = csvLine.Split(',');
+            // todo...
+
+        }
+
+        // 반드시 initialize 로 값 설정 이후 호출
+        public void reload()
+        {
+            setLastProfit3();
+            setPresentTotalStockPrise();
+            setPER();
+            setEPS();
+            setFutureStockPrice();
+            setValuation();
+        }
+
+        // 평균순익(직전년도) = 영엽이익(LastProfit1) + 당기순익(LastProfit2) / 2
+        private void setLastProfit3()
+        {
+            LastProfit3 = (LastProfit1 + LastProfit2) / 2;
+        }
+
+        // 현재 시가총액 = 상장주식수(LastStockCount) x 종가(LastPrise)
+        private void setPresentTotalStockPrise()
+        {
+            PresentTotalStockPrise = LastStockCount * LastPrise;
+        }
+
+        // PER1(영업이익) = 현재 시가총액(PresentTotalStockPrise) / 영업이익(LastProfit1)
+        // PER2(당기순익) = 현재 시가총액(PresentTotalStockPrise) / 당기순익(LastProfit1)
+        // PER3(평균순익) = 현재 시가총액(PresentTotalStockPrise) / 평균순익(LastProfit1)
+        private void setPER()
+        {
+            PER1 = PresentTotalStockPrise / LastProfit1;
+            PER2 = PresentTotalStockPrise / LastProfit2;
+            PER3 = PresentTotalStockPrise / LastProfit3;
+
+            if (UserPER == 0)
+            {
+                UserPER = PER3;
+            }
+        }
+
+        // EPS(영업이익) = 영업이익(LastProfit1) / 상장주식수(LastStockCount)
+        private void setEPS()
+        {
+            EPS1 = LastProfit1 / LastStockCount;
+            EPS2 = LastProfit2 / LastStockCount;
+            EPS3 = LastProfit3 / LastStockCount;
+        }
+
+        // 미래주가(영업이익) = EPS(영업이익) x UserPER
+        // 미래주가(당기순익) = EPS(당기순익) x UserPER
+        // 미래주가(평균순익) = EPS(평균순익) x UserPER
+        private void setFutureStockPrice()
+        {
+            FutureStockPrice1 = EPS1 * UserPER;
+            FutureStockPrice2 = EPS2 * UserPER;
+            FutureStockPrice3 = EPS3 * UserPER;
+        }   
+
+        // 기업평가(영업이익), true: 매수(종가 < 미래주가(영업이익)), false: 고평가
+        // 주가차이(영업이익) = 미래주가(영업이익) - 종가
+        private void setValuation()
+        {
+            ValuationPrise1 = FutureStockPrice1 - LastPrise;
+            ValuationBuy1 = (LastPrise < FutureStockPrice1) ? true : false;
+            ValuationPrise2 = FutureStockPrice2 - LastPrise;
+            ValuationBuy2 = (LastPrise < FutureStockPrice1) ? true : false;
+            ValuationPrise3 = FutureStockPrice3 - LastPrise;
+            ValuationBuy3 = (LastPrise < FutureStockPrice1) ? true : false;
+        }
+
         public void displayConsole()
         {
             Console.WriteLine("==================================================");
@@ -100,7 +181,9 @@ namespace OpenStock
             Console.WriteLine("LastProfit3: {0}", LastProfit3);
             Console.WriteLine("PresentTotalStockPrise: {0}", PresentTotalStockPrise);
             Console.WriteLine("FutureTotalStockPrise: {0}", FutureTotalStockPrise);
-            Console.WriteLine("LastPER: {0}", LastPER);
+            Console.WriteLine("PER1: {0}", PER1);
+            Console.WriteLine("PER2: {0}", PER2);
+            Console.WriteLine("PER3: {0}", PER3);
             Console.WriteLine("UserPER: {0}", UserPER);
             Console.WriteLine("EPS1: {0}", EPS1);
             Console.WriteLine("EPS2: {0}", EPS2);
